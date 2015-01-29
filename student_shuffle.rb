41b327132b32207787ab564b6214d484d2238107
Jennifer Krohn <jenniferlynn1207@msn.com>
2015-01-29 09:52:32 -0500

    added shuffle

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,6 +1,9 @@
 #Write a program that splits the class into teams of 2. Even teams if everyone shows up, last team will have
 #3 if odd number.
 
-students = ['Daniele,' 'Alesia,' 'Anita,' 'Ben,' 'Thomas,' 'Matthew,' 'Mark,' 'Jennifer,' 'Cameron']
+students = ['Daniele', 'Alesia', 'Anita', 'Ben', 'Thomas', 'Matthew', 'Mark', 'Jennifer', 'Cameron']
 
+puts students
+" "
+students.shuffle!
 puts students
