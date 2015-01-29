231d5a72592cde13d3b03d3fe7fc93b9f5ade98c
Jennifer Krohn <jenniferlynn1207@msn.com>
2015-01-29 09:42:39 -0500

    first commit

---
+++ student_shuffle.rb
@@ -0,0 +1,6 @@
+#Write a program that splits the class into teams of 2. Even teams if everyone shows up, last team will have
+#3 if odd number.
+
+students = ['Daniele,' 'Alesia,' 'Anita,' 'Ben,' 'Thomas,' 'Matthew,' 'Mark,' 'Jennifer,' 'Cameron']
+
+puts students
