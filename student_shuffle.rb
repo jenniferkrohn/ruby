a2b11f6b6690baed6ed8c957f617fe433b50b805
Jennifer Krohn <jenniferlynn1207@msn.com>
2015-01-29 10:05:43 -0500

    get students paired

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -7,3 +7,17 @@ puts students
 " "
 students.shuffle!
 puts students
+
+#set a count
+count = 0
+num_students = students.length - 1
+
+#until count is the number of students
+until count == num_students
+
+#test loop
+puts "#{students[count]} & #{students[count+1]} " #count is the index, 'count 0 is Daniele'
+
+# increase the count
+count += 2 #by changing from 1 to 2, increments every two students to print in pairs
+end
